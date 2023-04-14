4b88b4e6b797b6a649668daf2bd85dd7e43de9af
carldelfin <22149864+carldelfin@users.noreply.github.com>
2023-04-14 11:06:02 +0200

    Minor changes

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # mullvad-vpn-status
 
-Shell script that checks [Mullvad](https://mullvad.net/en) connection status and allows the user to toggle connections. It also checks how many days the account has left, and warns the user when the account will expire within a week. Intended for use with [Polybar](https://github.com/polybar/polybar). 
+Shell script that checks [Mullvad](https://mullvad.net/en) connection status and how many days the account has left, and warns the user when the account will expire within a week. Intended for use with [Polybar](https://github.com/polybar/polybar). 
 
 ## Usage
 
-Download the script and make sure it's executable (`chmod +x mullvad_vpn_status.R`). You'll also need [Font Awesome](https://fontawesome.com/) if you want icons. Then add something like this to your `user_modules.ini`:
+Download the script and make sure it's executable (`chmod +x mullvad_vpn_status.sh`). You'll also need [Font Awesome](https://fontawesome.com/) if you want icons. Then add something like this to your `user_modules.ini`:
 
 ```
 [module/vpn]
--- mullvad_vpn_status.sh
+++ mullvad_vpn_status.sh
@@ -1,37 +1,30 @@
 #!/bin/sh
 
-vpn_status=$(mullvad status | cut -d ' ' -f3)
-ip_address=$(mullvad status | awk 'match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/){print substr($0,RSTART,RLENGTH)}')
-protocol=$(mullvad status | awk '{for(i=1;i<=NF;i++)if($i=="to")print $(i+1)}')
+vpn_status=$(mullvad status | cut -d ' ' -f1)
+
+# datediff based on:
+# https://stackoverflow.com/a/59548761
+
+datediff() {
+    d1=$(date -d "$1" +%s)
+    d2=$(date -d "$2" +%s)
+    echo $(( (d1 - d2) / 86400 ))
+}
+
 account_expiry_date=$(mullvad account get | sed -n 3p | awk '{print $4}')
 current_date=$(date +%Y-%m-%d $now)
-days_left=$(($account_expiry_date - $current_date));
-
-echo $account_expiry_date
-echo $current_date
-echo $days_left
+days_left=$(datediff "${account_expiry_date}" "${current_date}")
 
 vpn_report() {
     if [ "$vpn_status" = "Connected" ]; then
         if [ $days_left -lt 7 ]; then
-            echo "%{F#e87e96} ($protocol | $days_left days left!)%{F-}"
+            echo "%{F#e87e96} ($days_left days left!)%{F-}"
         else
-            echo "%{F#eceff4} ($protocol | $days_left days left)%{F-}"
+            echo "%{F#eceff4} ($days_left days left)%{F-}"
         fi
 	else
 		echo "%{F#ebcb8b} %{F-}"
 	fi
 }
 
-vpn_toggle_connection() {
-    if [ "$vpn_status" = "Connected" ]; then
-        mullvad disconnect
-    else
-        mullvad connect
-    fi
-}
-
-case "$1" in
-    --toggle-connection) vpn_toggle_connection ;;
-	*) vpn_report ;;
-esac
+vpn_report
