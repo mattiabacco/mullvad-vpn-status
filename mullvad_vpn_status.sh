f53e5a71b10663ca24e08bd492c9f719e4a56a90
mattia <mattiabacco95@gmail.com>
2025-05-15 11:55:56 +0200

    fix: Compatibility with new Mullvad version feat: Add Nerdfont Icon and Catpuccin palette colors

--- mullvad_vpn_status.sh
+++ mullvad_vpn_status.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 vpn_status=$(mullvad status | cut -d ' ' -f1)
-
 # datediff based on:
 # https://stackoverflow.com/a/59548761
 
@@ -11,19 +10,20 @@ datediff() {
     echo $(( (d1 - d2) / 86400 ))
 }
 
-account_expiry_date=$(mullvad account get | sed -n 3p | awk '{print $4}')
+#account_expiry_date=$(mullvad account get | sed -n 3p | awk '{print $4}')
+account_expiry_date=$(mullvad account get | sed -n 2p | awk '{print $3}') 
 current_date=$(date +%Y-%m-%d $now)
 days_left=$(datediff "${account_expiry_date}" "${current_date}")
 
 vpn_report() {
     if [ "$vpn_status" = "Connected" ]; then
         if [ $days_left -lt 7 ]; then
-            echo "%{F#e87e96} ($days_left days left!)%{F-}"
+            echo "%{F#fab387}󰀑 ($days_left days left!)%{F-}"
         else
-            echo "%{F#eceff4}%{F-}"
+            echo "%{F#a6e3a1}󰛳%{F-}"
         fi
 	else
-		echo "%{F#ebcb8b}%{F-}"
+		echo "%{F#f38ba8}󰅛%{F-}"
 	fi
 }
 
